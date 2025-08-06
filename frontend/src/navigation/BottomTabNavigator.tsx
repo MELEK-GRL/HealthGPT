fa6e289e49f38bea0eb97f7d63ce8312e395d490
MELEK <100144640+MELEK-GRL@users.noreply.github.com>
2025-08-06 20:37:19 +0300

    icon eklendi

--- frontend/src/navigation/BottomTabNavigator.tsx
+++ frontend/src/navigation/BottomTabNavigator.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/Ionicons';
 import Chat from '../screen/chat';
 import User from '../screen/user/User';
 import History from '../screen/history';
@@ -8,10 +9,29 @@ const Tab = createBottomTabNavigator();
 
 const BottomTabNavigator = () => {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName = '';
+
+          if (route.name === 'Chat') {
+            iconName = focused ? 'chatbubble' : 'chatbubble-outline';
+          } else if (route.name === 'History') {
+            iconName = focused ? 'time' : 'time-outline';
+          } else if (route.name === 'User') {
+            iconName = focused ? 'person' : 'person-outline';
+          }
+
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#007aff',
+        tabBarInactiveTintColor: 'gray',
+      })}
+    >
       <Tab.Screen name="Chat" component={Chat} />
       <Tab.Screen name="History" component={History} />
-       <Tab.Screen name="User" component={User} />
+      <Tab.Screen name="User" component={User} />
     </Tab.Navigator>
   );
 };
